e2bed8904767151a0c103afffb26ed4e785ec4e9
Lesly Notter <notter.lesly@gmail.com>
2020-12-27 13:40:23 +0100

    Changed slides show

--- public/modules/projects.js
+++ public/modules/projects.js
@@ -1,34 +1,30 @@
-slide = document.getElementById("slide");
+var slideIndex = 1;
+showSlides(slideIndex);
 
-bolinha1 = document.getElementById("bolinha1");
-bolinha1.addEventListener("click", ativa1);
-
-function ativa1() {
-  slide.classList.remove("f2", "f3", "f4");
-  slide.classList.add("f1");
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-bolinha2 = document.getElementById("bolinha2");
-bolinha2.addEventListener("click", ativa2);
-
-function ativa2() {
-  slide.classList.remove("f1", "f3", "f4");
-  slide.classList.add("f2");
+function currentSlide(n) {
+  showSlides((slideIndex = n));
 }
 
-bolinha3 = document.getElementById("bolinha3");
-bolinha3.addEventListener("click", ativa3);
-
-function ativa3() {
-  slide.classList.remove("f1", "f2", "f4");
-
-  slide.classList.add("f3");
-}
-
-bolinha4 = document.getElementById("bolinha4");
-bolinha4.addEventListener("click", ativa4);
-function ativa4() {
-  slide.classList.remove("f1", "f2", "f3");
-
-  slide.classList.add("f4");
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
