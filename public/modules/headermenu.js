e41a6233dddd470e777231a82e094a5347647d37
Lesly Notter <notter.lesly@gmail.com>
2020-12-27 13:49:14 +0100

    Changed slide show

--- public/modules/headermenu.js
+++ public/modules/headermenu.js
@@ -3,27 +3,20 @@ const skills = document.getElementById("skills");
 const projects = document.getElementById("projects");
 const contact = document.getElementById("contact");
 
-about.addEventListener("click", descepouco);
+about.addEventListener("click", () => {
+  scrollDown(".about");
+});
+skills.addEventListener("click", () => {
+  scrollDown(".skills");
+});
+projects.addEventListener("click", () => {
+  scrollDown(".project");
+});
+contact.addEventListener("click", () => {
+  scrollDown(".contact");
+});
 
-function descepouco() {
-  window.scrollTo(0, 800);
-}
-
-skills.addEventListener("click", descemedio), 800;
-
-function descemedio() {
-  window.scrollTo(0, 1400);
-
-}
-
-projects.addEventListener("click", descemuito);
-
-function descemuito() {
-  window.scrollTo(0, 2000);
-}
-
-contact.addEventListener("click", desce);
-
-function desce() {
-  window.scrollTo(0, document.body.scrollHeight);
+function scrollDown(className) {
+  let el = document.querySelector(className);
+  el.scrollIntoView(true);
 }
